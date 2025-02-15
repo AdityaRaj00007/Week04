273ea993526bae953e2f81f56d14c55582fa4dd1
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-15 20:18:23 +0530

    [Aditya] Add . Parameterized Test and test cases also available

---
+++ src/main/java/com/tit/regexandjunit/junit/basicjunittesting/parameterizedtest/Parameterized.java
@@ -0,0 +1,6 @@
+package com.tit.regexandjunit.junit.basicjunittesting.parameterizedtest;
+public class Parameterized {
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ src/test/java/basicjunittesting/parameterizedtesting/TestParameterizedTest.java
@@ -0,0 +1,20 @@
+package basicjunittesting.parameterizedtesting;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import static org.junit.jupiter.api.Assertions.*;
+import com.tit.regexandjunit.junit.basicjunittesting.parameterizedtest.Parameterized;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TestParameterizedTest {
+    static Parameterized parameterized;
+    @BeforeAll
+    static void setUp(){
+        parameterized = new Parameterized();
+    }
+    @ParameterizedTest
+    @ValueSource (ints = {2 , 4 , 6 ,7 ,9})
+    void testEvenNumbers(int number){
+        assertTrue(parameterized.isEven(number));
+    }
+}
