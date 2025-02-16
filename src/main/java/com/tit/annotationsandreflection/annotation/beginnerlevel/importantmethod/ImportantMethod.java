11e153c2d7519650f422ca00e70406f3f94b5868
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-16 23:45:47 +0530

    [Aditya] Add . Create ImportantMethod annotation and retrieve annotated methods using Reflection

---
+++ src/main/java/com/tit/annotationsandreflection/annotation/beginnerlevel/importantmethod/ImportantMethod.java
@@ -0,0 +1,10 @@
+package com.tit.annotationsandreflection.annotation.beginnerlevel.importantmethod;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface ImportantMethod {
+    String level() default "HIGH";
+}
