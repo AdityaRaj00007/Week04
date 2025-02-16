97e68a9bca096a1f4fe276165be1c69a95eeb722
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-16 23:35:51 +0530

    [Aditya] Add . Implement dynamic proxy for logging method calls

---
+++ src/main/java/com/tit/annotationsandreflection/reflection/advancedlevel/customlogging/Greeting.java
@@ -0,0 +1,5 @@
+package com.tit.annotationsandreflection.reflection.advancedlevel.customlogging;
+
+interface Greeting {
+    void sayHello();
+}
---
+++ src/main/java/com/tit/annotationsandreflection/reflection/advancedlevel/customlogging/GreetingImpl.java
@@ -0,0 +1,8 @@
+package com.tit.annotationsandreflection.reflection.advancedlevel.customlogging;
+
+class GreetingImpl implements Greeting {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
